f5d811a4b4a36e09b9e61747380bb7da288db573
Josue Mejia <josueomejiap@gmail.com>
2023-08-09 19:37:11 -0600

    cambios hechos en clase 090823 de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './empleados/empleados.component';
+import { EmpleadoComponent } from './empleado/empleado.component';
+import { EmpleadoFeoComponent } from './empleado-feo/empleado-feo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, EmpleadosComponent, EmpleadoComponent, EmpleadoFeoComponent
   ],
   imports: [
     BrowserModule
